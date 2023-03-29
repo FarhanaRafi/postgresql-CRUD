7f72196effba52b4aefa8712ac4c2718fad21de0
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-29 15:14:28 +0200

    endpoints done

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import createHttpError from "http-errors";
 import ProductsModel from "./model.js";
+import { Op } from "sequelize";
 
 const productsRouter = express.Router();
 
@@ -14,7 +15,13 @@ productsRouter.post("/", async (req, res, next) => {
 });
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const products = await ProductsModel.findAll();
+    const query = {};
+    if (req.query.name) query.name = { [Op.iLike]: `%${req.query.name}%` };
+    if (req.query.minPrice && req.query.maxPrice)
+      query.price = { [Op.between]: [req.query.minPrice, req.query.maxPrice] };
+    const products = await ProductsModel.findAndCountAll({
+      where: { ...query },
+    });
     res.send(products);
   } catch (error) {
     next(error);
@@ -22,18 +29,58 @@ productsRouter.get("/", async (req, res, next) => {
 });
 productsRouter.get("/:productId", async (req, res, next) => {
   try {
+    const product = await ProductsModel.findByPk(req.params.productId, {
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    if (product) {
+      res.send(product);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 productsRouter.put("/:productId", async (req, res, next) => {
   try {
+    const [numberOfUpdatedRows, updatedRecords] = await ProductsModel.update(
+      req.body,
+      { where: { productId: req.params.productId }, returning: true }
+    );
+    if (numberOfUpdatedRows === 1) {
+      res.send(updatedRecords[0]);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 productsRouter.delete("/:productId", async (req, res, next) => {
   try {
+    const numberOfDeletedRows = await ProductsModel.destroy({
+      where: { productId: req.params.productId },
+    });
+    if (numberOfDeletedRows === 1) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
