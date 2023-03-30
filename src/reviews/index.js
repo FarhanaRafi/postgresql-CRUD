896a86f2b61f838d2a5b1a65a209ee9dbee50c63
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-30 16:46:51 +0200

    done

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -29,4 +29,48 @@ reviewsRouter.get("/:productId/reviews", async (req, res, next) => {
   }
 });
 
+reviewsRouter.put("/:productId/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const [numberOfUpdatedRows, updatedRows] = await ReviewsModel.update(
+      req.body,
+      { where: { reviewId: req.params.reviewId }, returning: true }
+    );
+    if (numberOfUpdatedRows === 1) {
+      res.send(updatedRows[0]);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Review with id ${req.params.reviewId} was not found!`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewsRouter.delete(
+  "/:productId/reviews/:reviewId",
+  async (req, res, next) => {
+    try {
+      const numberOfDeletedReviews = await ReviewsModel.destroy({
+        where: { reviewId: req.params.reviewId },
+      });
+      if (numberOfDeletedReviews === 1) {
+        res.status(204).send();
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Review with id ${req.params.reviewId} was not found!`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default reviewsRouter;
