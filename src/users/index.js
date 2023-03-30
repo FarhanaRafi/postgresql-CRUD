1fa64035f6991ba4d80a6035924eb19b7fa3c359
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-03-30 16:28:07 +0200

    done

--- src/users/index.js
+++ src/users/index.js
@@ -24,18 +24,49 @@ usersRouter.get("/", async (req, res, next) => {
 });
 usersRouter.get("/:userId", async (req, res, next) => {
   try {
+    const user = await UsersModel.findByPk(req.params.userId, {
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    }); // attributes could be an array (when you want to pass a list of the selected fields), or an object (with the exclude property, whenever you want to pass a list of omitted fields)
+    if (user) {
+      res.send(user);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 usersRouter.put("/:userId", async (req, res, next) => {
   try {
+    const [numberOfUpdatedRows, updatedRecords] = await UsersModel.update(
+      req.body,
+      { where: { userId: req.params.userId }, returning: true }
+    );
+    if (numberOfUpdatedRows === 1) {
+      res.send(updatedRecords[0]);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 usersRouter.delete("/:userId", async (req, res, next) => {
   try {
+    const numberOfDeletedRows = await UsersModel.destroy({
+      where: { userId: req.params.userId },
+    });
+    if (numberOfDeletedRows === 1) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
